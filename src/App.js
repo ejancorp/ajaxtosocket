bd546a9eda2768fce910210cac9e4238dbd82447
Ej <ejan.corp@gmail.com>
2018-03-03 09:02:19 +0400

    update spacing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import socketio from 'socket.io';
 import Poll from './Modules/Poll';
 import Request from './Modules/Request';
 
-class AjaxToPoll {
-  // const method = Request.get.bind(null, 'https://api.fixer.io/latest');
-  // const App = new Poll(method, (result) => {
-  //   console.log(result);
-  // }, 3000);
-  //
-  // App.start();
+class AjaxToSocket {
+// static start(url, callback = () => {}) {
+//   const method = Request.get.bind(null, url);
+//   const App = new Poll(method, (result) => {
+//     console.log(result);
+//   }, 3000);
+//
+//   App.start();
+// }
+
 }
 
-export default AjaxToPoll;
+export default AjaxToSocket;
